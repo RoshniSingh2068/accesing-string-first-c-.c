0b36f25b4c33ec1a6375f5a49c21f7e08276bc6d
RoshniSingh2068 <roshnisingh1228@gmail.com>
2024-08-26 10:58:36 +0530

    Create accesing string first c++.c

---
+++ accesing string first c++.c
@@ -0,0 +1,12 @@
+// you can access the string by referring ti its index number inside the square bracket
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+    string mystring = "Hello";
+    cout << mystring[0];
+    return 0;
+
+}
